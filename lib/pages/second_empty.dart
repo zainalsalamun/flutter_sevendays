caef05a8b05b3a5dc52bd81f62654671a613e6e3
zainal salamun <zainalrtf@gmail.com>
2022-07-25 15:01:55 +0700

    second empty & second started

---
+++ lib/pages/second_empty.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class SecondEmpty extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/pages/second_started.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class SecondStarted extends StatelessWidget {
+  const SecondStarted({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.only(
+          top: 60,
+          left: 60,
+          right: 40,
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Healthy Fisrt.',
+              style: GoogleFonts.poppins(
+                color: Colors.black,
+                fontSize: 25,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+            SizedBox(
+              height: 16,
+            ),
+            Text(
+              'Exercise toogether with our best \ncommunity fit in the world',
+              style: GoogleFonts.poppins(
+                color: Color(0xff828284),
+                fontSize: 16,
+              ),
+            ),
+            SizedBox(height: 60),
+            Image.asset(
+              'assets/gallery.png',
+              width: 295,
+              height: 402,
+            ),
+            SizedBox(height: 70),
+            Container(
+              width: 295,
+              height: 55,
+              color: Color(0xffAFEA0D),
+              child: TextButton(
+                onPressed: () {},
+                child: Text(
+                  'Shape My Body',
+                  style: GoogleFonts.lato(
+                    color: Colors.black,
+                    fontSize: 18,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(height: 20),
+            Center(
+              child: Text(
+                'Terms & Conditions',
+                style: GoogleFonts.poppins(
+                  color: Color(0xff757575),
+                  fontSize: 16,
+                  decoration: TextDecoration.underline,
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
